c1bab8837d4240ed21d787bf9faa631700dbd95a
tiffanyv185 <tiffanyv185@gmail.com>
2024-07-03 22:40:49 +1000

    added detailed comments to all functions in data_manager.py

--- src/modules/data_manager.py
+++ src/modules/data_manager.py
@@ -1,16 +1,16 @@
-import json
-import os
+import json # module used for encoding and decoding JSON data
+import os # module used for interacting with the operating system, in this case, to check if the JSON file exists
 
 FILE_PATH = "saving_user_data.json" # Define the file path for saving data
 
 
 def load_data(): # Load data from file
-    if os.path.exists(FILE_PATH): #load data from JSON file if it exists, otherwise return default data
+    if os.path.exists(FILE_PATH): # check if file JSON file exists
         with open(FILE_PATH, 'r') as file:
-            return json.load(file)
-    return {"goals": [], "contributions": []} 
+            return json.load(file) # if the file exists, it is opened in read mode and contents is loaded using json.load(file)
+    return {"goals": [], "contributions": []} # if JSON file does not exist, a default list structure with headings "goals" and "contributions" is returned
 
 
-def save_data(data): # Save data to file
-    with open(FILE_PATH, "w") as file:
-        json.dump(data, file)
+def save_data(data): # Save data to JSON file
+    with open(FILE_PATH, "w") as file: # the file is opened in write monde
+        json.dump(data, file) #the data os dumped into the file
